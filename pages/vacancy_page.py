f98222dc69b1353c029d8d7325c51aee34acbe42
Alina Kazeka <ayukazeka@gmail.com>
2023-09-21 20:34:19 +0300

    add page file, change test logic via using class

---
+++ pages/vacancy_page.py
@@ -0,0 +1,47 @@
+from selene import browser, have, be
+
+
+class VacancyPage:
+    def open(self):
+        browser.open(
+            'https://t-systems.jobs/global-careers-en/jobs/gr/209045/Software-Engineer-for-Automation-_-TDI/Athens.html'
+        )
+        browser.element('#consentAcceptAll').click()
+
+    def job_title_should_have_text(self, job_title):
+        browser.element('#ja-job-title').should(have.text(job_title))
+
+    def work_requirements_should_have_facts(self, skill, work_mode, location):
+        browser.element('.ja-facts').all('.btn-light').should(
+            have.exact_texts(f'{skill}', f'{work_mode}', f'{location}')
+        )
+
+    def work_requirements_should_have_more_facts(
+        self, experience, travel, language, service, less_btn
+    ):
+        browser.element('.ja-section-more-facts').all('.btn-light').should(
+            have.exact_texts(
+                f'{experience}',
+                f'{travel}',
+                f'{language}',
+                f'{service}',
+                f'{less_btn}',
+            )
+        )
+
+    def click_more_button(self):
+        browser.element('#facts-show-more').click()
+
+    def asser_name_tasks_block(self, block_name):
+        browser.element('#headline-our-offer').should(have.exact_text(block_name))
+
+    def click_show_more_button(self):
+        browser.element('.show-more').click()
+
+    def asser_new_line(self, new_line):
+        browser.element('#info-our-offer > ul > li:nth-child(8)').should(
+            have.exact_text(new_line)
+        )
+
+    def panel_img_is_visivle(self):
+        browser.element('.ja-panel-image').should(be.visible)
--- tests/test_vacany.py
+++ tests/test_vacany.py
@@ -1,43 +1,36 @@
-import pytest
 from selene import browser, have, be
+from pages.vacancy_page import VacancyPage
 
 
 def test_page():
-    browser.open(
-        'https://t-systems.jobs/global-careers-en/jobs/gr/209045/Software-Engineer-for-Automation-_-TDI/Athens.html'
-    )
-    browser.element('#consentAcceptAll').click()
+    vacancy_page = VacancyPage()
+
+    vacancy_page.open()
 
     # check job-title
-    browser.element('#ja-job-title').should(
-        have.text('SOFTWARE ENGINEER FOR AUTOMATION - TDI')
-    )
+    vacancy_page.job_title_should_have_text('SOFTWARE ENGINEER FOR AUTOMATION - TDI')
 
     # check header
-    browser.element('.ja-facts').all('.btn-light').should(
-        have.exact_texts('Professionals', 'Regular (Part Time possible)', 'Athens')
+    vacancy_page.work_requirements_should_have_facts(
+        'Professionals', 'Regular (Part Time possible)', 'Athens'
     )
 
     # check 'more' btn
-    browser.element('#facts-show-more').click()
-    browser.element('.ja-section-more-facts').all('.btn-light').should(
-        have.exact_texts(
-            '3-5 years Experience Required',
-            '0-25% Amount of Travel',
-            'English',
-            'Deutsche Telekom Cloud Services s.r.o.',
-            'less',
-        )
+    vacancy_page.click_more_button()
+    vacancy_page.work_requirements_should_have_more_facts(
+        '3-5 years Experience Required',
+        '0-25% Amount of Travel',
+        'English',
+        'Deutsche Telekom Cloud Services s.r.o.',
+        'less',
     )
 
     # check 'your task' block
-    browser.element('#headline-our-offer').should(have.exact_text('Your tasks'))
+    vacancy_page.asser_name_tasks_block('Your tasks')
 
     # ckeck 'Show more' btn
-    browser.element('.show-more').click()
-    browser.element('#info-our-offer > ul > li:nth-child(8)').should(
-        have.exact_text('    Working in international teams across Europe.')
-    )
+    vacancy_page.click_show_more_button()
+    vacancy_page.asser_new_line('    Working in international teams across Europe.')
 
     # check image
-    browser.element('.ja-panel-image').should(be.visible)
+    vacancy_page.panel_img_is_visivle()
